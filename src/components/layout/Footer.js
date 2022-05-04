bab1d9381fe0e5b88008291451283879a5902898
Anderson Kardek <kardek.anderson@gmail.com>
2022-05-03 21:30:03 -0400

    Add: Finalizando Home e ajustando Footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,8 +6,12 @@ function Footer() {
   return(
     <footer className={styles.footer}>
       <ul className={styles.social_list}>
-        <li><FaGithub /></li>
-        <li><FaLinkedin /></li>
+        <li>
+          <a href="https://github.com/AndersonKardek" target="_blank" rel="noreferrer"> {<FaGithub />}</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/anderson-kardek-6357b6176/" target="_blank" rel="noreferrer"> {<FaLinkedin />}</a>
+        </li>
       </ul>
       <p className={styles.copy_right}><span>Costs</span> &copy; 2022</p>
     </footer>
